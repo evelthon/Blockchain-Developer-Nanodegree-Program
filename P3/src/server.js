a42e56e0a87fecde702c60e7e5d48b211f365eed
Evelthon Prodromou <epro@prodromou.eu>
2018-08-14 16:56:19 +0300

    New: /GET route (get block by block ID) New: /POST route (add new block to the chain)

--- P3/src/server.js
+++ P3/src/server.js
@@ -1,5 +1,9 @@
 'use strict';
 
+//Import Blockchain class and instantiate
+const bChain = require('../simpleChain');
+const chain = new bChain();
+
 const Hapi=require('hapi');
 
 // Create a server with a host and port
@@ -15,8 +19,40 @@ server.route({
     handler:function(request,h) {
 
         return'hello world';
+
     }
 });
+
+/*
+Request block with block ID
+If the block ID is omitted, return genesis block.
+ */
+server.route({
+    method:'GET',
+    path:'/block/{block_id?}',
+    handler:function(request,h) {
+
+        const bid = request.params.block_id ?
+            encodeURIComponent(request.params.block_id) :
+            '0';
+
+        console.log(chain.getBlock(0))
+        return `fetching block ${bid}`;
+    }
+});
+
+//POST block (add new block)
+server.route({
+    method:'POST',
+    path:'/block',
+    handler:function(request,h) {
+
+        const {block} = request.payload;
+
+        return `Posting finished`;
+    }
+});
+
 
 // Start the server
 async function start() {
