a8658d8d386c3edfdb7ea15dcb53a9c0fa266a35
Evelthon Prodromou <epro@prodromou.eu>
2018-08-14 12:23:46 +0300

    Minor: Migrate files from Project 2.

---
+++ P3/README.md
@@ -0,0 +1,27 @@
+# Blockchain Data
+
+Blockchain has the potential to change the way that the world approaches data. Develop Blockchain skills by understanding the data model behind Blockchain by developing your own simplified private blockchain.
+
+## Getting Started
+
+These instructions will install requirements and allow you to execute the code.
+### Prerequisites
+
+Installing Node and NPM is pretty straightforward using the installer package available from the (Node.js® web site)[https://nodejs.org/en/]. Most Linux distributions have ready-to-install packages.
+
+### Configuring your project
+
+
+- Install requirements
+```
+npm install 
+```
+- Execute program
+```
+node simpleChain.js
+```
+
+After successful execution you should end up with:
+ - A Genesis Block (#0)
+ - 20 additional Blocks
+ - A validation confirmation of the Blockchain
---
+++ P3/leveldbfunctions.js
@@ -0,0 +1,56 @@
+/*
+    Helper function to interact with LevelDB
+ */
+
+const level = require('level');
+const chainDB = './chaindata';
+const db = level(chainDB);
+
+var exports = module.exports = {};
+
+//Add block
+exports.addBlock = function (key, value) {
+    return new Promise(function (resolve, reject) {
+        db.put(key, value, function (err) {
+            if (err) reject('Block ' + key + ' submission failed', err);
+            resolve('Added block ' + key + ' to the chain');
+        })
+    });
+
+
+};
+
+//get Block height
+exports.getMaxHeight = function () {
+    return new Promise(function (resolve, reject) {
+        let chainLength = 0;
+        let keyStream = db.createKeyStream();
+        keyStream.on('data', function (data) {
+            chainLength += 1;
+        })
+            .on('error', function(err) {
+                reject(err);
+            })
+            .on('close', function () {
+                //When the stream is finished, return found max height.
+                resolve(chainLength);
+            });
+    });
+
+}
+
+
+//Get block
+exports.getBlock = function (key) {
+    // key = key.toString()
+    return new Promise(function (resolve, reject) {
+        db.get(key, function (err, value) {
+            if (err) reject('Block ' + key + ' not found!');
+            // console.log('LEVEL Value = ' + value);
+            // console.log("LEVEL " + typeof  value);
+            // console.log("LEVEL " + typeof JSON.parse(value));
+            resolve(value);//returns a string
+        })
+    })
+
+}
