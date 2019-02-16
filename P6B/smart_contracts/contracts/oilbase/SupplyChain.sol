e8c83a8745aaa3d02ceb941b4c443a8271dbb84c
Evelthon Prodromou <epro@prodromou.eu>
2019-02-16 17:44:05 +0200

    Added missing semicolons

--- P6B/smart_contracts/contracts/oilbase/SupplyChain.sol
+++ P6B/smart_contracts/contracts/oilbase/SupplyChain.sol
@@ -96,43 +96,43 @@ contract SupplyChain {
 
     // Define a modifier that checks if an item.state of a upc is Processed
     modifier extracted(uint _upc) {
-        require(items[_upc].itemState == State.Extracted)
+        require(items[_upc].itemState == State.Extracted);
         _;
     }
 
     // Define a modifier that checks if an item.state of a upc is Packed
     modifier packed(uint _upc) {
-        require(items[_upc].itemState == State.Packed)
+        require(items[_upc].itemState == State.Packed);
         _;
     }
 
     // Define a modifier that checks if an item.state of a upc is ForSale
     modifier forSale(uint _upc) {
-        require(items[_upc].itemState == State.ForSale)
+        require(items[_upc].itemState == State.ForSale);
         _;
     }
 
     // Define a modifier that checks if an item.state of a upc is Sold
     modifier sold(uint _upc) {
-        require(items[_upc].itemState == State.Sold)
+        require(items[_upc].itemState == State.Sold);
         _;
     }
 
     // Define a modifier that checks if an item.state of a upc is Shipped
     modifier shipped(uint _upc) {
-        require(items[_upc].itemState == State.Shipped)
+        require(items[_upc].itemState == State.Shipped);
         _;
     }
 
     // Define a modifier that checks if an item.state of a upc is Received
     modifier received(uint _upc) {
-        require(items[_upc].itemState == State.Received)
+        require(items[_upc].itemState == State.Received);
         _;
     }
 
     // Define a modifier that checks if an item.state of a upc is Purchased
     modifier purchased(uint _upc) {
-        require(items[_upc].itemState == State.Purchased)
+        require(items[_upc].itemState == State.Purchased);
         _;
     }
 
@@ -181,7 +181,7 @@ contract SupplyChain {
             originFarmLongitude: _originFarmLongitude,
 
             productNotes: _productNotes
-            })
+            });
 
         // Increment sku
         sku = sku + 1;
