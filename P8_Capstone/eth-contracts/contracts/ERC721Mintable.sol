72432708fce65cd53879d61d1f3de8a760466e59
Evelthon Prodromou <epro@prodromou.eu>
2019-03-30 15:10:34 +0200

    Implemented ERC721Metadata (part of)

--- P8_Capstone/eth-contracts/contracts/ERC721Mintable.sol
+++ P8_Capstone/eth-contracts/contracts/ERC721Mintable.sol
@@ -494,8 +494,12 @@ contract ERC721Enumerable is ERC165, ERC721 {
 contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 
     // TODO: Create private vars for token _name, _symbol, and _baseTokenURI (string)
+    string private _name;
+    string private _symbol;
+    string private _baseTokenURI;
 
     // TODO: create private mapping of tokenId's to token uri's called '_tokenURIs'
+    mapping(uint256 => string) _tokenURIs;
 
     bytes4 private constant _INTERFACE_ID_ERC721_METADATA = 0x5b5e139f;
     /*
@@ -507,12 +511,26 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 
 
     constructor (string memory name, string memory symbol, string memory baseTokenURI) public {
-        // TODO: set instance var values
+        // TODO: set instance var
+        _name = name;
+        _symbol = symbol;
+        _baseTokenURI = baseTokenURI;
 
         _registerInterface(_INTERFACE_ID_ERC721_METADATA);
     }
 
     // TODO: create external getter functions for name, symbol, and baseTokenURI
+    function name() external view returns (string memory){
+        return _name;
+    }
+
+    function symbol() external view returns (string memory){
+        return _symbol;
+    }
+
+    function baseTokenURI() external view returns (string memory){
+        return _baseTokenURI;
+    }
 
     function tokenURI(uint256 tokenId) external view returns (string memory) {
         require(_exists(tokenId));
