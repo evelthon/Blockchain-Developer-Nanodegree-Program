c13a0bc541d43d07e7738d13eccb3082ba080ebf
Evelthon Prodromou <epro@prodromou.eu>
2019-04-01 15:19:02 +0300

    tests for ERC721Mintable (completed)

--- P8_Capstone/eth-contracts/test/TestERC721Mintable.js
+++ P8_Capstone/eth-contracts/test/TestERC721Mintable.js
@@ -4,7 +4,10 @@ contract('TestERC721Mintable', accounts => {
 
     const account_one = accounts[0];
     const account_two = accounts[1];
-    const account_three = accounts[3];
+    const account_three = accounts[2];
+    const account_four = accounts[3];
+    const account_five = accounts[4];
+    const account_six = accounts[5];
 
     describe('have ownership properties', function () {
         beforeEach(async function () {
@@ -81,8 +84,19 @@ contract('TestERC721Mintable', accounts => {
 
         });
 
-        it('XXX verify approval for all tokens', async function () {
+        it('verify approval for all tokens', async function () {
+            let isApproved = await this.contract.isApprovedForAll(account_one, account_three, {from: account_one});
+            assert.equal(isApproved, false, "account_three should not be approved");
 
+            //approve account_three for all
+            await this.contract.setApprovalForAll(account_three, true, {from:account_one});
+            isApproved = await this.contract.isApprovedForAll(account_one, account_three, {from: account_one});
+            assert.equal(isApproved, true, "account_three should be approved");
+
+            //remove approval for all
+            await this.contract.setApprovalForAll(account_three, false, {from:account_one});
+            isApproved = await this.contract.isApprovedForAll(account_one, account_three, {from: account_one});
+            assert.equal(isApproved, false, "account_three should (again) not be approved for all");
         });
 
 
@@ -94,22 +108,66 @@ contract('TestERC721Mintable', accounts => {
             this.contract = await ERC721MintableComplete.new({from: account_one});
 
             // TODO: mint multiple tokens
+            await this.contract.mintVerifiedTokenTo(account_two, 0, {from: account_one});
+            await this.contract.mintVerifiedTokenTo(account_three, 1, {from: account_one});
+            await this.contract.mintVerifiedTokenTo(account_four, 2, {from: account_one});
+            await this.contract.mintVerifiedTokenTo(account_five, 3, {from: account_one});
+            await this.contract.mintVerifiedTokenTo(account_six, 4, {from: account_one});
         })
 
         it('should return total supply', async function () {
-
+            let result = await this.contract.totalSupply.call({from: account_one});
+            assert.equal(result.toNumber(), 5, "Error calculating total supply");
         })
 
         it('should get token balance', async function () {
-
+            let result = await this.contract.balanceOf.call(account_five, {from:account_one});
+            assert.equal(result.toNumber(), 1, "Wrong balance for account_five");
         })
 
         // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
         it('should return token uri', async function () {
-
+            let result = await this.contract.tokenURI.call(1, {from: account_one});
+            assert.equal(result, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1", "Error fetching token uri");
         })
 
         it('should transfer token from one owner to another', async function () {
+            const tokenId = 0;
+
+            //is token approved?
+            let approvedAddress = await this.contract.getApproved.call(tokenId, {from:account_one});
+            assert.equal(parseInt(approvedAddress), 0, "Should not be able to retrieve an approved token");
+
+            //add approval for account_four
+            await this.contract.approve(account_four, tokenId, {from: account_two});
+            approvedAddress = await this.contract.getApproved.call(tokenId, {from: account_one});
+            assert.equal(approvedAddress, account_four, "This should be approved for account_four");
+
+            //verify current owner
+            let owner = await this.contract.ownerOf.call(tokenId, {from: account_one});
+            assert.equal(owner, account_two, "Owner is not account_two");
+
+            /*
+            Check balances of account_thw & account_six
+            transfer from account_two to account_six
+            Check new balances of account_thw & account_six
+             */
+
+            let accountBalance = await this.contract.balanceOf.call(account_two, {from: account_one});
+            assert.equal(accountBalance.toNumber(), 1, "Error in account balance for account_two");
+
+
+            accountBalance = await this.contract.balanceOf.call(account_six, {from: account_one});
+            assert.equal(accountBalance.toNumber(), 1, "Error in account balance for account_six");
+
+            await this.contract.transferFrom(account_two, account_six, tokenId, {from: account_two});
+
+            accountBalance = await this.contract.balanceOf.call(account_two, {from: account_one});
+            assert.equal(accountBalance.toNumber(), 0, "Error in account balance for account_two (should be zero)");
+
+
+            accountBalance = await this.contract.balanceOf.call(account_six, {from: account_one});
+            assert.equal(accountBalance.toNumber(), 2, "Error in account balance for account_six (should be two");
 
         })
     });
